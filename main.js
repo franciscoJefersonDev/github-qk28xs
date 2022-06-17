fb466a7a6a5994fa5975f1ad126f2b60b236372b
franciscoJeferson <jfranciscojefersondossantosfre@gmail.com>
2022-06-17 19:33:56 -0300

    2 files modified, 3 added

--- main.js
+++ main.js
@@ -66,20 +66,3 @@ const generateColors = () => {
   }, 100);
 };
 generateColors();
-/* TO HSLA */
-// console.log(
-//   `hsla(${parseInt(chroma.random().hsl()[0])}, ${parseInt(
-//     chroma.random().hsl()[1]
-//   )}, ${parseInt(chroma.random().hsl()[2])}, ${parseInt(
-//     chroma.random().hsl()[3]
-//   )})`
-// );
-
-/* TO HEX */
-// console.log(chroma.random().hex());
-/* TO RGBA */
-// console.log(
-//   `rgba(${chroma.random().rgba()[0]}, ${chroma.random().rgba()[1]}, ${
-//     chroma.random().rgba()[2]
-//   }, ${chroma.random().rgba()[3]})`
-// );
