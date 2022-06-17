41fb82ef3a16bc69e750ed36b08316b8ee2f91b9
franciscoJeferson <jfranciscojefersondossantosfre@gmail.com>
2022-06-17 14:54:26 -0300

    2 files modified

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+let colors = [];
 let config = {
   quantitieColors: 20,
   type: 'hex',
@@ -9,7 +10,7 @@ document
     config.quantitieColors = Number(event.target.value.trim());
   });
 document.querySelector('#typeColor').addEventListener('change', (event) => {
-  console.log(event.target.querySelector('[selected]').dataset.typeColor);
+  config.type = event.target.value;
 });
 document
   .querySelector('[data-click="generateColors"]')
@@ -17,11 +18,32 @@ document
     generateColors();
   });
 
+const generateHexColor = () => chroma.random().hex();
+const generateRgbaColor = () =>
+  `rgba(${chroma.random().rgba()[0]}, ${chroma.random().rgba()[1]}, ${
+    chroma.random().rgba()[2]
+  }, ${chroma.random().rgba()[3]})`;
+const generateHslaColor = () =>
+  `hsla(${parseInt(chroma.random().hsl()[0])}, ${parseInt(
+    chroma.random().hsl()[1]
+  )}, ${parseInt(chroma.random().hsl()[2])}, ${parseInt(
+    chroma.random().hsl()[3]
+  )})`;
+
 const generateColors = () => {
-  for (let i = config.quantitieColors; i > 0; i--) {
-    console.log(i);
+  colors = [];
+  for (let i = 1; i <= config.quantitieColors; i++) {
+    if (config.type === 'hex') {
+      colors.push(generateHexColor());
+    } else if (config.type === 'rgba') {
+      colors.push(generateRgbaColor());
+    } else {
+      colors.push(generateHslaColor());
+    }
   }
+  console.log(colors);
 };
+generateColors();
 /* TO HSLA */
 // console.log(
 //   `hsla(${parseInt(chroma.random().hsl()[0])}, ${parseInt(
