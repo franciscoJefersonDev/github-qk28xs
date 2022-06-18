772d5094b431d54e367f58ffb6a92df09106f822
franciscoJeferson <jfranciscojefersondossantosfre@gmail.com>
2022-06-18 18:07:50 -0300

    2 files modified

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let colors = [];
 let config = {
   quantitieColors: 20,
   type: 'hex',
+  hue: 'random',
 };
 const paletteColor = JSON.parse(localStorage.getItem('palette-color')) || [];
 
@@ -13,6 +14,9 @@ document
 document.querySelector('#typeColor').addEventListener('change', (event) => {
   config.type = event.target.value;
 });
+document.querySelector('#hue').addEventListener('change', (event) => {
+  config.hue = event.target.value;
+});
 document
   .querySelector('[data-click="generateColors"]')
   .addEventListener('click', (event) => {
@@ -22,82 +26,73 @@ document
 const generateHexColor = () => chroma.random().hex();
 
 const generateColors = () => {
-  colors = [];
+  colors = randomColor({
+    count: config.quantitieColors,
+    hue: config.hue,
+    format: config.type,
+  });
   const insertColors = document.querySelector('.insert-colors');
   insertColors.innerHTML = '';
-  const spinner = document.createElement('div');
-  const spinnerMessage = document.createElement('span');
-  spinnerMessage.textContent = 'Loading...';
-  spinner.classList.add('spinner-border', 'text-primary');
-  spinner.setAttribute('role', 'status');
-  spinnerMessage.classList.add('visually-hidden');
-  spinner.appendChild(spinnerMessage);
-  insertColors.appendChild(spinner);
-  setTimeout(() => {
-    for (let i = 1; i <= config.quantitieColors; i++) {
-      colors.push(generateHexColor());
-    }
-    insertColors.removeChild(spinner);
-    colors.forEach((color) => {
-      const col = document.createElement('div');
-      const card = document.createElement('div');
-      const cardBody = document.createElement('div');
-      const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
-      col.classList.add('col-3', 'my-1', 'mx-1');
-      card.classList.add('card');
-      card.style.backgroundColor = color;
-      cardBody.classList.add('card-body');
-      card.appendChild(cardBody);
-      col.appendChild(card);
-      insertColors.appendChild(col);
-      const alert = (message, type) => {
-        const wrapper = document.createElement('div');
-        wrapper.innerHTML = [
-          `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-          `   <div>${message}</div>`,
-          '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
-          '</div>',
-        ].join('');
+  colors.forEach((color) => {
+    const col = document.createElement('div');
+    const card = document.createElement('div');
+    const cardBody = document.createElement('div');
+    const alertPlaceholder = document.getElementById('liveAlertPlaceholder');
+    col.classList.add('col-3', 'my-1', 'mx-1');
+    card.classList.add('card');
+    card.style.backgroundColor = color;
+    cardBody.classList.add('card-body');
+    card.appendChild(cardBody);
+    col.appendChild(card);
+    insertColors.appendChild(col);
+    const alert = (message, type) => {
+      const wrapper = document.createElement('div');
+      wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>',
+      ].join('');
 
-        alertPlaceholder.append(wrapper);
-      };
-      const mc = new Hammer(card);
-      mc.on('tap press', (event) => {
-        if (event.type === 'tap') {
-          const typeColor =
-            config.type === 'hex'
-              ? chroma(color).hex()
-              : config.type === 'rgba'
-              ? chroma(color).rgba()
-              : chroma(color).hsl();
-          navigator.clipboard
-            .writeText(typeColor)
-            .then(() => {
-              alert('Copied!', 'primary');
-            })
-            .catch((err) => {
-              alert('Error!', 'danger');
-            });
-        } else if (event.type === 'press') {
-          const test = saveColorInPalette(color);
-          if (test) {
-            alert('Saved!', 'primary');
-          } else {
-            alert('This color already exists!', 'danger');
-          }
+      alertPlaceholder.append(wrapper);
+    };
+    const mc = new Hammer(card);
+    mc.on('tap press', (event) => {
+      if (event.type === 'tap') {
+        const typeColor =
+          config.type === 'hex'
+            ? chroma(color).hex()
+            : config.type === 'rgba'
+            ? chroma(color).rgba()
+            : chroma(color).hsl();
+        navigator.clipboard
+          .writeText(typeColor)
+          .then(() => {
+            alert('Copied!', 'primary');
+          })
+          .catch((err) => {
+            alert('Error!', 'danger');
+          });
+      } else if (event.type === 'press') {
+        const test = saveColorInPalette(color);
+        if (test) {
+          alert('Saved!', 'primary');
+        } else {
+          alert('This color already exists!', 'danger');
         }
-      });
+      }
     });
-  }, 100);
+  });
 };
 generateColors();
 const saveColorInPalette = (color) => {
   alreadyExisty = paletteColor.includes(color);
-  if (!alreadyExisty) {
-    paletteColor.push(color);
-    localStorage.setItem('palette-color', JSON.stringify(paletteColor));
-    return true;
-  } else {
-    return false;
-  }
+  console.log(color);
+  // if (!alreadyExisty) {
+  //   paletteColor.push(color);
+  //   localStorage.setItem('palette-color', JSON.stringify(paletteColor));
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 };
