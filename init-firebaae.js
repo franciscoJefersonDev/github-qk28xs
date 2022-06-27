96fbd221e0457c3014c80f75885ea0fcb6709578
franciscoJeferson <jfranciscojefersondossantosfre@gmail.com>
2022-06-27 04:33:00 -0300

    1 file modified, 1 added

---
+++ init-firebaae.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWUr9_fRIDMuR09vfiuZ2PvOIyb6Fm59Q",
+  authDomain: "todo-list-c3c29.firebaseapp.com",
+  projectId: "todo-list-c3c29",
+  storageBucket: "todo-list-c3c29.appspot.com",
+  messagingSenderId: "656082746137",
+  appId: "1:656082746137:web:10d1feb25e8a0ad74ee388"
+};
+
+firebase.initializeApp(firebaseConfig)
